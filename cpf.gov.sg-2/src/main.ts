c36297bbf6471920e373fa4a2d84c106493f49a2
zhaoyan <zhaoyan.x_gds@scientecpeople.com>
2025-07-05 14:49:57 +0800

    Formatting fit

--- cpf.gov.sg-2/src/main.ts
+++ cpf.gov.sg-2/src/main.ts
@@ -1,5 +1,5 @@
 import { frontendTemplate } from "../../frontend-template";
 import { API_DOMAIN } from "./paths";
 
-const main = frontendTemplate(API_DOMAIN, "CPFxxx");
+const main = frontendTemplate(API_DOMAIN, "CPF");
 main();
